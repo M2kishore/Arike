a86c8118033807ad896bcef5f11c3124b9ce4da8
M M Kishore <kishoremeenakshi2001@gmail.com>
2022-03-10 11:42:33 +0530

    login need to tweak more created Patient view

--- user/views.py
+++ user/views.py
@@ -1,80 +1,85 @@
 from msilib.schema import ListView
 from re import template
+from sre_constants import SUCCESS
+from django.forms import ModelForm, TextInput, CharField, PasswordInput
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
-from django.contrib.auth.models import User
+from user.models import Patient, User
 from django.contrib.auth.views import LoginView
-from django.views.generic.edit import CreateView
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.contrib.auth import login as auth_login
+from django.utils.translation import gettext, gettext_lazy as _
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    UserChangeForm,
+    UsernameField,
+)
 
-from django.forms import ModelForm
 
-# from user.models import User
-
-# Create your views here.
 class HomeView(View):
     def get(self, request):
         return render(request, "base.html")
 
 
-# class UserLoginForm(ModelForm):
-#     class Meta:
-#         model = Faculty
-#         fields = ("user", "role")
+class UserCreateView(CreateView):
+    form_class = UserCreationForm
+    template_name = "signup.html"
+    success_url = "dashboard/"
+
+class UseUpdateForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('full_name','phone','email')
+
+class UserUpdateView()
+
+# class UserLoginForm(AuthenticationForm):
+#     username = UsernameField(widget=TextInput(attrs={"autofocus": True}))
+#     password = CharField(
+#         label=_("Password"),
+#         strip=False,
+#         widget=PasswordInput(attrs={"autocomplete": "current-password"}),
+#     )
 
 
 class UserLoginView(LoginView):
     template_name = "login.html"
-    success_url = "dashboard/"
-
-    def form_valid(self,form):
-        self.object = form.save()
-
-def LoginPostView(request):
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return HttpResponseRedirect("dashboard/")
-        # Redirect to a success page.
-        ...
-    else:
-        return render(request,"login.html")
-
-def LoginView(request):
-    return render(request,"login.html")
-        # Return an 'invalid login' error message.
-
-    # def get(self,request):
-    #     return render(request, "login.html")
-
-    # def post(self,request):
-    #     email = request.POST.get("email")
-    #     password = request.POST.get("password")
-    #     user = Users.objects.get(email=email,password=password)
-    #     if user:
-    #         return HttpResponseRedirect("dashboard/")
-    #     else:
-    #         return HttpResponseRedirect("login/")
-
-# class UserCreateForm(ModelForm):
-#     class Meta:
-#         model = User
-#         fields = ["username","email","password1","password2"]
 
 
-class GenericUserCreateView(CreateView):
-    form_class = UserCreationForm
-    template_name = "signup.html"
-    success_url = "login/"
+class PatientForm(ModelForm):
+    class Meta:
+        model = Patient
+        fields = "__all__"
 
 
-class GenericDashboardView(CreateView):
-    pass
-#Admin Views
+class CreatePatientView(CreateView):
+    form_class = PatientForm
+    template_name = "patient_create.html"
+    success_url = "patients/"
 
-class GenericCreateFacilityView(CreateView):
-    pass
+
+class UpdatePatientView(UpdateView):
+    model = Patient
+    form_class = PatientForm
+    template_name = "patient_update.html"
+    success_url = "patients/"
+
+    def get_queryset(self):
+        return Patient.objects.filter(deleted=False, nurse=self.request.user)
+
+
+class DeletePatientView(DeleteView):
+    model = Patient
+    template_name = "patient_delete.html"
+    success_url = "patients/"
+
+
+class DetailPatientView(DetailView):
+    model = Patient
+    template_name = "patient_detail.html"
+
+    def get_queryset(self):
+        return Patient.objects.filter(deleted=False, nurse=self.request.user)
