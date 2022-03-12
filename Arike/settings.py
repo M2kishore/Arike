88e154df2c20430a3541be64c9571c6f84751340
M M Kishore <kishoremeenakshi2001@gmail.com>
2022-03-13 00:47:55 +0530

    heroku deployment

--- Arike/settings.py
+++ Arike/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*8*=+3!5y=daw#1()r*z&*)*d2yw#0s-3-za(5e-)x$c^2w74t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["m2kishore-arike.herokuapp.com"]
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
